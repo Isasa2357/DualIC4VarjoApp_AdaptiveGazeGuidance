75ac1e4d209d74955f0ba6be9ca3d726940f1737
Isasa2357 <Isasa_EGwork@outlook.com>
2026-07-11 16:45:31 +0900

    Reset application configuration for minimal display stage

--- src/AppConfig.hpp
+++ src/AppConfig.hpp
@@ -20,40 +20,25 @@ struct CameraAppConfig {
     std::optional<int> offsetY;
 };
 
-struct CalibrationAppConfig {
-    bool enabled = false;
-    std::optional<std::filesystem::path> jsonPath;
-
-    std::uint32_t boardColumns = 12;
-    std::uint32_t boardRows = 9;
-    std::string profile = "affine_vertical";
-    bool profileExplicit = false;
-    std::size_t maxObservations = 30;
-    std::size_t minObservations = 8;
-    double minCornerMotionPx = 15.0;
-    double ransacThresholdPx = 1.5;
-    bool useChessboardSb = false;
-};
-
 struct AppConfig {
     CameraAppConfig left;
     CameraAppConfig right;
-    CalibrationAppConfig calibration;
 
     int width = 0;
     int height = 0;
-    double fps = 0.0;
+    double fps = 160.0;
     IC4Ext::CameraPixelFormat inputFormat = IC4Ext::CameraPixelFormat::BGR8;
     bool formatExplicit = false;
 
     double syncToleranceMs = 5.0;
-    IC4Ext::FrameSyncTimestampSource timestampSource = IC4Ext::FrameSyncTimestampSource::HostReceived;
+    IC4Ext::FrameSyncTimestampSource timestampSource =
+        IC4Ext::FrameSyncTimestampSource::HostReceived;
     std::size_t syncBufferedFramesPerCamera = 16;
     std::size_t inputQueueSize = 64;
     std::size_t outputQueueSize = 8;
 
     std::uint32_t cameraReadTimeoutMs = 1000;
-    std::uint32_t cameraStartDelayMs = 2000;
+    std::uint32_t cameraStartDelayMs = 0;
     std::uint32_t initialFrameTimeoutMs = 15000;
 
     float planeWidthMeters = 1.0f;
@@ -64,7 +49,7 @@ struct AppConfig {
     VarjoXR::PlacementMode placementMode = VarjoXR::PlacementMode::HeadRelative;
 
     std::size_t displayRingSize = 4;
-    bool enableD3D12DebugLayer = true;
+    bool enableD3D12DebugLayer = false;
     double maxRuntimeSeconds = 0.0;
 
     std::filesystem::path outputBaseDirectory;
@@ -75,8 +60,11 @@ struct AppConfig {
 
 bool ParseArguments(int argc, char** argv, AppConfig& config, std::string& error);
 void PrintUsage(std::ostream& out);
-IC4Ext::CameraCaptureConfig MakeCaptureConfig(const AppConfig& app, const CameraAppConfig& camera);
+IC4Ext::CameraCaptureConfig MakeCaptureConfig(
+    const AppConfig& app,
+    const CameraAppConfig& camera);
 std::filesystem::path MakeDefaultMetadataPath();
-const char* TimestampSourceName(IC4Ext::FrameSyncTimestampSource source) noexcept;
+const char* TimestampSourceName(
+    IC4Ext::FrameSyncTimestampSource source) noexcept;
 
 } // namespace DualIC4Varjo
